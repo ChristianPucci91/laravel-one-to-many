ffc39b3f07d7a343ad0643242aacc4440107500f
ChristianPucci91 <pucci.christian91@gmail.com>
2021-02-05 19:39:03 +0100

    creata tabella typology NaM

--- database040221/database/migrations/2021_02_05_182027_create_task_typology_table.php
+++ database040221/database/migrations/2021_02_05_182027_create_task_typology_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddForeignKeys extends Migration
+class CreateTaskTypologyTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class AddForeignKeys extends Migration
      */
     public function up()
     {
-        Schema::table('tasks', function (Blueprint $table) {
+        Schema::create('task_typology', function (Blueprint $table) {
+            $table->id();
 
-           $table -> foreign('employee_id' , 'task-emp')
-                  -> references('id')
-                  -> on('employees');
-
+            $table-> bigInteger('task_id') -> unsigned();
+            $table-> bigInteger('typology_id') -> unsigned();
+            $table->timestamps();
         });
     }
 
@@ -29,12 +29,6 @@ class AddForeignKeys extends Migration
      */
     public function down()
     {
-
-      Schema::table('tasks', function (Blueprint $table) {
-
-        $table -> dropForeign('task-emp');
-
-      });
-
+        Schema::dropIfExists('task_typology');
     }
 }
